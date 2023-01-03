dc73d2b9de1da1e02558cb3c1b70f7a3e0561434
Aish Fenton <aish.fenton@gmail.com>
2023-01-03 18:00:22 +0000

    increase timeout moar

--- src/test/scala/afenton/bazel/bsp/End2EndTest.scala
+++ src/test/scala/afenton/bazel/bsp/End2EndTest.scala
@@ -24,7 +24,7 @@ import scala.reflect.Typeable
 
 class End2EndTest extends munit.CatsEffectSuite with BspHelpers:
 
-  override val munitTimeout = 1.minute
+  override val munitTimeout = 2.minute
 
   val projectRoot = Paths.get("").toAbsolutePath
 
